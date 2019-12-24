647e6902625f9e6c5063bb2cc438eeb3a5513193
Vladimir Bogachev <vladimir_bogachev@yahoo.com>
2019-12-24 16:39:59 +0600

    Initial Commit of Infrastructure Dependency Project

---
+++ RabbitMQMicro/RabbitMQMicro.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RabbitMQMicro.Domain.Core.Bus;
+using RabbitMQMicro.Infra.Bus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMQMicro.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services) 
+        {
+            //Domain bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
